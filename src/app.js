d3a5406075550f66fac7fc1a16486a3853c15165
LostBready <darkman.chel228@gmail.com>
2026-03-01 20:00:34 +0300

    added communitating with backend, cards, preloader rendering and some styles

--- src/app.js
+++ src/app.js
@@ -7,13 +7,24 @@ function app(){
         sortingType: "",
         searchingPrompt: "",
         error: "",
+        isLoading: false
     }
     const elements = {
         input: document.querySelector("input"),
         searchButton: document.querySelector("button"),
-        plusButton: document.querySelector("#plus"),
-        layout: document.querySelector("#cards"),
+        cards: document.querySelector("#cards"),
     }
     const watch = view(initialState, elements)
+
+    elements.searchButton.addEventListener('click', async ()=>{
+        if (! watch.isLoading){
+            watch.isLoading = true
+            const searchText = elements.input.value
+            const dataRequest = await fetch(`http://www.omdbapi.com/?apikey=&s=${searchText}`)
+            const data = await dataRequest.json()
+            watch.isLoading = false
+            watch.cards = data.Search
+        }
+    })
     }
 app()
--- src/view.js
+++ src/view.js
@@ -1,8 +1,65 @@
 import onChange from "on-change"
 function view(state, elements){
     const watch = onChange(state, (path, value) => {
-        console.log(path, value)
+        switch(path){
+            case 'cards':
+                renderCards(value, elements.cards)
+                break
+            case 'isLoading':
+                clearCards(elements.cards)
+                value ? showPreloader(elements.searchButton) : removePreloader(elements.searchButton)
+                break
+        }
     })
     return watch
 }
+
+const clearCards = (cardsDiv) => {
+    cardsDiv.innerHTML = ''
+}
+
+const showPreloader = (buttonDiv) => {
+    buttonDiv.disabled = true
+    const preloaderDiv = document.createElement('div')
+    preloaderDiv.classList.add('loader')
+    document.body.append(preloaderDiv)
+}
+
+const removePreloader = (buttonDiv) => {
+    buttonDiv.disabled = false
+    const preloaderDiv = document.querySelector('.loader')
+    preloaderDiv.remove()
+}
+
+const renderCards = (filmsArr, cardsDiv) => {
+    filmsArr.forEach(({Title, Year, Type, Poster}) => {
+        const filmCard = createCard(Title, Poster, Type, Year)
+        cardsDiv.append(filmCard)
+    });
+}
+
+const createCard = (titleText, linkImage, type, yearText) => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const title = document.createElement('h2');
+  const desc = document.createElement('p');
+  const year = document.createElement('p');
+
+  div.classList.add('card');
+  desc.classList.add('desc');
+  year.classList.add('year');
+
+  title.textContent = titleText;
+  img.setAttribute('src', linkImage);
+  desc.textContent = type;
+  year.textContent = yearText;
+
+  div.append(img);
+  div.append(title);
+  div.append(desc);
+  div.append(year);
+
+  return div;
+}
+
 export default view
